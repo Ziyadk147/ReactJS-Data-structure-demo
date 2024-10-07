81110cbb922320fd6f381ee0999340f19e45b172
Ziyadk147 <Ziyadkhan2333@gmail.com>
2024-10-07 10:50:36 +0500

    main screen responsive fixed

--- src/Pages/Startscreen/Startscreen.jsx
+++ src/Pages/Startscreen/Startscreen.jsx
@@ -31,32 +31,33 @@ function Startscreen({onClick}) {
                     initial={{y: -1000, opacity: 0}}
                     animate={{y: 0, opacity: 1}}
                     transition={{duration: 2}}
-                    className="flex flex-row  justify-center items-center h-screen w-full"
+                    className="flex flex-row  justify-center items-center h-screen "
                 >
                     <div className="flex flex-col justify-center items-center">
-                        <h1 className="font-bold text-stone-100 text-4xl">Please Select any of the following options</h1>
+                        <h1 className="font-bold text-stone-100 text-4xl lg:px-0 px-3">Please Select any of the following options</h1>
                         <motion.ul
                             initial={{y: 1000, opacity: 0}}
                             animate={{y: 0, opacity: 1}}
                             transition={{duration: 2}}
-                            className={"flex flex-row my-6 "}
+                            className={"flex lg:flex-row flex-col my-6  "}
                         >
-                            <li className={"flex flex-col px-2"}>
-                                <button className={"rounded-3xl  bg-yellow-500 w-28 font-bold h-14"}
-                                    onClick={ ()  => {onClick("stack")}}
+                            <li className={"flex lg:flex-col flex-row mx-2"}>
+                                <button className={"rounded-3xl  bg-yellow-500 w-full lg:m-0 my-1  px-10 font-bold h-14"}
+                                        onClick={ ()  => {onClick("stack")}}
+
                                 >
                                     Stack
                                 </button>
                             </li>
-                            <li className={"flex flex-col px-2"}>
-                                <button className={"rounded-3xl bg-yellow-500 w-28 font-bold h-14"}
+                            <li className={"flex lg:flex-col flex-row mx-2"}>
+                                <button className={"rounded-3xl bg-yellow-500 lg:m-0 my-1  w-full px-10 font-bold h-14"}
                                     onClick={ ()  => {onClick("queue")}}
                                 >
                                     Queue
                                 </button>
                             </li>
-                            <li className={"flex flex-col px-2"}>
-                                <button className={"rounded-3xl bg-yellow-500 w-28 font-bold h-14"}
+                            <li className={"flex lg:flex-col flex-row mx-2"}>
+                                <button className={"rounded-3xl bg-yellow-500  w-full lg:m-0 my-1 px-10 font-bold h-14"}
                                     onClick={ ()  => {onClick("linkedlist")}}
                                 >
                                     Linked List
