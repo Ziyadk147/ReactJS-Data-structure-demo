80b9943a670f8c4eb1d6e2f1cc1392a800cd6336
Ziyadk147 <Ziyadkhan2333@gmail.com>
2024-10-09 16:10:46 +0500

    minor fixes

--- src/Pages/QueuePage/QueuePage.jsx
+++ src/Pages/QueuePage/QueuePage.jsx
@@ -42,7 +42,7 @@ A queue is a linear data structure that follows the First In, First Out (FIFO) p
                     initial={{y: -100, opacity: 0}}
                     animate={{y: 0, opacity: 1}}
                     transition={{duration: 2}}
-                    className={"flex  justify-center items-center "}
+                    className={"flex lg:flex-row flex-col  justify-center items-center "}
                 >
                     <h1 className="text-4xl font-bold text-stone-100">Queue Data Structure</h1>
 
@@ -50,7 +50,7 @@ A queue is a linear data structure that follows the First In, First Out (FIFO) p
             )}
             {triggerChange && (
                 <>
-                    <div className={"flex lg:flex-row flex-col justify-center items-center w-screen lg:h-fit h-screen "}>
+                    <div className={"flex lg:flex-row flex-col justify-center items-center w-screen "}>
                         <Queue Queue={queueArray}/>
                         <AnimatedButtonList buttonList={buttonList}/>
                         <AnimatedDescription>{queueText}</AnimatedDescription>
